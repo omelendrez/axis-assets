52a7ae0014cf727d3b266109127223c467d554a6
Omar Melendrez <omar.melendrez@gmail.com>
2023-04-30 16:42:53 -0300

    Move arrow function call before execution

--- app/index.js
+++ app/index.js
@@ -4,21 +4,6 @@ require('dotenv').config()
 const { log } = require('./helpers/log.js')
 const logger = require('morgan')
 const multer = require('multer')
-const FILE_PATH = 'uploads'
-const upload = multer({
-  dest: `${FILE_PATH}/`,
-  limits: {
-    files: 5, // allow up to 5 files per request,
-    fileSize: 2 * 1024 * 1024 // 2 MB (max file size)
-  },
-  fileFilter: (req, file, cb) => {
-    // allow images only
-    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
-      return cb(new Error('Only image are allowed.'), false)
-    }
-    cb(null, true)
-  }
-})
 
 const app = express()
 
@@ -33,6 +18,23 @@ app.use(
     skip: (req, res) => res.statusCode < 400
   })
 )
+
+const FILE_PATH = 'uploads'
+
+const upload = multer({
+  dest: `${FILE_PATH}/`,
+  limits: {
+    files: 5, // allow up to 5 files per request,
+    fileSize: 2 * 1024 * 1024 // 2 MB (max file size)
+  },
+  fileFilter: (req, file, cb) => {
+    // allow images only
+    if (!file.originalname.match(/\.(jpg|jpeg|png|gif)$/)) {
+      return cb(new Error('Only image are allowed.'), false)
+    }
+    cb(null, true)
+  }
+})
 
 app.post('/upload-photo', upload.single(upload), async (req, res) => {
   try {
