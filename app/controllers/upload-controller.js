d0e5e1951f3c90b423d6ac2ff1ee5053c9a27c9e
Omar Melendrez <omar.melendrez@gmail.com>
2023-06-30 14:10:09 -0300

    Add previous FOET certificate upload feature

--- app/controllers/upload-controller.js
+++ app/controllers/upload-controller.js
@@ -199,6 +199,7 @@ exports.uploadPreviousFOET = async (req, res) => {
 
     sharp(inputFile)
       .withMetadata()
+      // .rotate(-90, { background: { r: 0, g: 0, b: 0, alpha: 0 } })
       .resize({
         width: parseInt(process.env.FOET_WIDTH, 10),
         height: parseInt(process.env.FOET_HEIGHT, 10)
