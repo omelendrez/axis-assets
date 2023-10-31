d3c4ac5703a5923f46edd695f1aa58e8d80eab1c
Omar Melendrez <omar.melendrez@gmail.com>
2023-10-31 13:36:16 -0300

    Implement validate email address and send to learner with admin as sender

--- app/controllers/email-controller.js
+++ app/controllers/email-controller.js
@@ -3,6 +3,7 @@ const path = require('node:path')
 
 const EmailService = require('../services/EmailService')
 const { log } = require('../helpers/log')
+const isValidEmail = require('../helpers/validations')
 
 exports.sendWelcomeLetter = async (req, res) => {
   try {
@@ -11,10 +12,16 @@ exports.sendWelcomeLetter = async (req, res) => {
 
     const { to, course, filename, full_name: fullName } = message
 
+    if (!isValidEmail(to)) {
+      return res.status(400).send({
+        message: `Invalid email address [${to}]`
+      })
+    }
+
     const { name: courseName } = course
 
     const body = `<p>Dear <b>${fullName}</b>,</p>
-    <p>Thank you for registering for the <b>${courseName}</b> Course at Tolmann Allied Services Company Limited</p>
+    <p>Thank you for registering for the <b>${courseName}</b> Course at Tolmann Allied Services Company Limited.</p>
     <p>You can find attached a document with the respective instructions for the Course.
     <br/><br/>
     <p><b>Management.</b>
@@ -34,7 +41,7 @@ exports.sendWelcomeLetter = async (req, res) => {
 
     const email = {
       to,
-      cc: 'nwabuezestephen27@gmail.com',
+      bcc: process.env.SMTP_SERVER_BCC,
       subject: 'COURSE JOINING INSTRUCTIONS',
       html: body,
       attachments: [
