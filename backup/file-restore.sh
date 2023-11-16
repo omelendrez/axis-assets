fb2dc6806c0141b3d1bfc5a7cc7af9a864c69c08
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-16 10:31:35 -0300

    Add backup files

--- backup/file-restore.sh
+++ backup/file-restore.sh
@@ -1,19 +1,3 @@
-folder_group="exports"
-
-echo "$folder_group"
-while read p; do
-  echo " - $p"
-
- tar -Pxzf "$folder_group-$p.tar.gz"
-
-done < $folder_group-folders-list.txt
-
-folder_group="uploads"
-
-echo "$folder_group"
-while read p; do
-  echo " - $p"
-   tar -Pxzf "$folder_group-$p.tar.gz"
-done < $folder_group-folders-list.txt
+tar -Pxzf --skip-old-files "*.tar.gz"
 
 rm *.gz
