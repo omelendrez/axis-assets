5457607596bd5459ed9437f885c9f4cdf86b9afc
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-16 11:56:32 -0300

    Add backup files

--- backup/file-backup.sh
+++ backup/file-backup.sh
@@ -1,23 +1,79 @@
 rm *.gz
 
+days=-60
+
+echo "Backup started..."
+echo
+
+echo "Generate files list to compress"
+
+echo
+
 folder_group="exports"
 
 echo "$folder_group"
-while read p; do
-  echo " - $p"
-  tar -cvjf "$folder_group-$p.tar.gz" --exclude="*.gitignore" "./../$folder_group/$p"
-done < $folder_group-folders-list.txt
+while read f; do
+
+  echo " - $f"
+
+  find "./../$folder_group/$f" -mtime $days $(printf "! -name %s " $(cat skip_files)) -not -path "./../$folder_group/$f" -and -not -path "./../$folder_group/$f/csv" >"$folder_group-$f"
+
+done <$folder_group-folders-list
 
 folder_group="uploads"
 
 echo "$folder_group"
-while read p; do
-  echo " - $p"
-  tar -cvjf "$folder_group-$p.tar.gz" --exclude="*.gitignore" "./../$folder_group/$p"
-done < $folder_group-folders-list.txt
+
+while read f; do
+
+  echo " - $f"
+
+  find "./../$folder_group/$f" -mtime $days $(printf "! -name %s " $(cat skip_files)) -not -path "./../$folder_group/$f" >"$folder_group-$f"
+
+done <"$folder_group-folders-list"
+
+echo
+
+echo "Compress selected files"
+echo
+folder_group="exports"
+
+echo "$folder_group"
+
+while read f; do
+
+  echo " - $f"
+
+  while read l; do
+
+    tar -cjf "$folder_group-$f.tar.gz" "$l"
+
+  done <"$folder_group-$f"
+
+done <"$folder_group-folders-list"
+
+folder_group="uploads"
+
+echo "$folder_group"
+
+while read f; do
+
+  echo " - $f"
+
+  while read l; do
+
+    tar -cjf "$folder_group-$f.tar.gz" "$l"
+
+  done <"$folder_group-$f"
+
+done <"$folder_group-folders-list"
+
+echo
+echo "Pushing files to GitHub repository."
+echo
 
 git add .
 git commit -m "Add backup files"
 git push
 
-
+echo "Backup process complete"
--- backup/file-restore.sh
+++ backup/file-restore.sh
@@ -1,3 +1,25 @@
-tar -Pxzf --skip-old-files "*.tar.gz"
+folder_group="exports"
+
+echo "$folder_group"
+
+while read p; do
+
+  echo " - $p"
+
+  tar -Pxzf "$folder_group-$p.tar.gz"
+
+done <"$folder_group-folders-list"
+
+folder_group="uploads"
+
+echo "$folder_group"
+
+while read p; do
+
+  echo " - $p"
+
+  tar -Pxzf "$folder_group-$p.tar.gz"
+
+done <"$folder_group-folders-list"
 
 rm *.gz
