fd0cf56143bc44da9d752257e0a9ef165ed1c2b2
Omar Melendrez <omar.melendrez@gmail.com>
2023-11-17 10:50:11 -0300

    Add backup files as of  17/11/2023 10:50

--- backup/backup-files.sh
+++ backup/backup-files.sh
@@ -5,78 +5,19 @@ rm *.$extension
 
 echo "Backup started..."
 
-echo
+while read root; do
 
-echo "Generate files list to compress"
+  while read folder; do
 
-echo
+    echo " - $folder"
 
-root="exports"
+    tar -Pcj --file="$root-$folder.$extension" -T "$root-$folder"
 
-echo "$root"
-while read folder; do
+  done <"$root-folders-list.txt"
 
-  echo " - $folder"
-
-  cd ..
-
-  find "$(pwd)/$root/$folder" -mtime $days $(printf "! -name %s " $(cat backup/skip_files)) -not -path "$root/$folder" -and -not -path "$root/$folder/csv" >"backup/$root-$folder"
-
-  cd backup
-
-done <"$root-folders-list"
-
-root="uploads"
+done <"root-folders-list.txt"
 
 echo
-
-echo "$root"
-
-while read folder; do
-
-  echo " - $folder"
-
-  cd ..
-
-  find "$(pwd)/$root/$folder" -mtime $days $(printf "! -name %s " $(cat backup/skip_files)) -not -path "$root/$folder" >"backup/$root-$folder"
-
-  cd backup
-
-done <"$root-folders-list"
-
-echo
-
-echo "Compress selected files"
-
-echo
-
-root="exports"
-
-echo "$root"
-
-while read folder; do
-
-  echo " - $folder"
-
-  tar -Pcz --file="$root-$folder.$extension" -T "$root-$folder"
-
-done <"$root-folders-list"
-
-echo
-
-root="uploads"
-
-echo
-
-echo "$root"
-
-while read folder; do
-
-  echo " - $folder"
-
-  tar -Pcz --file="$root-$folder.$extension" -T "$root-$folder"
-
-done <"$root-folders-list"
 
 echo
 echo "Pushing files to GitHub repository."
